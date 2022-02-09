be14f92e402cf1814a2a59f9f3a86497197c8a49
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-09 12:12:14 -0800

    GP solution

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express'); // importing a CommonJS module
-
+const morgan = require('morgan');
+const cors = require('cors');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(morgan('tiny'));
 
 server.use('/api/hubs', hubsRouter);
 
@@ -21,3 +24,8 @@ server.use('*', (req, res) => {
 });
 
 module.exports = server;
+
+function logger (req, res, next) {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+};
